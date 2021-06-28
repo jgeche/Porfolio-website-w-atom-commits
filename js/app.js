9c000075a09ed3fc9290c859d749e584dfdfdf81
jgeche <77022997+jgeche@users.noreply.github.com>
2021-06-28 10:33:18 -0700

    add changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// Letter Animation
-var textWrapper = document.querySelector('.ml2');
+// Letter Animation to Heading
+var textWrapper = document.querySelector('.heading');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: false})
   .add({
-    targets: '.ml2 .letter',
+    targets: '.heading .letter',
     scale: [4,1],
     opacity: [0,1],
     translateZ: 0,
@@ -18,3 +18,28 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+//Fixed Navbar that Disappears on DownScroll
+$(document).ready(function () {
+	var previousScroll = 0;
+	$(window).scroll(function () {
+		var currentScroll = $(this).scrollTop();
+		if (currentScroll < 100) {
+			showNav();
+		} else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+			if (currentScroll > previousScroll) {
+				hideNav();
+			} else {
+				showNav();
+			}
+			previousScroll = currentScroll;
+		}
+	});
+
+	function hideNav() {
+		$(".navbar").removeClass("is-visible").addClass("is-hidden");
+	}
+
+	function showNav() {
+		$(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+	}
+});
