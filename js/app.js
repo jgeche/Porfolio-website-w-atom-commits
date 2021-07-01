04dd6ebbab00e4532580ce0283c98da502fbe470
jgeche <77022997+jgeche@users.noreply.github.com>
2021-07-01 11:30:52 -0700

    changes to index and  cs and js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ anime.timeline({loop: false})
     easing: "easeOutExpo",
     duration: 1500,
     delay: (el, i) => 70*i
-  });
+});
 
 //Tooltip Window that Generates Text to Element
 $(function () {
@@ -21,25 +21,39 @@ $(function () {
 //Fixed Navbar that Disappears on DownScroll
 $(document).ready(function () {
 	var previousScroll = 0;
-	$(window).scroll(function () {
-		var currentScroll = $(this).scrollTop();
-		if (currentScroll < 100) {
-			showNav();
-		} else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
-			if (currentScroll > previousScroll) {
-				hideNav();
-			} else {
-				showNav();
-			}
-			previousScroll = currentScroll;
-		}
-	});
+$(window).scroll(function () {
+	var currentScroll = $(this).scrollTop();
+	if (currentScroll < 100) {
+	showNav();
+	} else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+	if (currentScroll > previousScroll) {
+	hideNav();
+	} else {
+	showNav();
+	}
+	previousScroll = currentScroll;
+	}
+});
 
-	function hideNav() {
-		$(".navbar").removeClass("is-visible").addClass("is-hidden");
+function hideNav() {
+	$(".navbar").removeClass("is-visible").addClass("is-hidden");
 	}
 
-	function showNav() {
-		$(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+function showNav() {
+	$(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
 	}
+});
+
+//Fade in Section on Mouse Scroll
+$(window).scroll(function() {
+    $('.fade-in').each(function() {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+            $(this).addClass('is-visible');
+        }
+    });
 });
